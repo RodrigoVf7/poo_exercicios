b4cdaf20c572b12f74fe9280bc361b4b3ae57d1f
Rodrigo Vinicius <rodrigovfsilva09@gmail.com>
2024-10-10 12:45:38 -0300

    Alteração feita!

--- Desktop/poo_exercicios/exercicios_1semana/Ex1.java
+++ Desktop/poo_exercicios/exercicios_1semana/Ex1.java
@@ -1,12 +1,17 @@
 package exercicios_1semana;
 import java.util.Scanner;
 
-public class Ex1{
+import java.util.Scanner;
+
+public class Ex1 {
     public static void main(String[] args) {
-        Scanner escreve = new Scanner(System.in);
-        System.out.println("Olá! DIGITE UM NÚMERO:");
-        int num = escreve.nextInt();
-        System.out.println(num + " é o número digitado.");
-        escreve.close();
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite o primeiro número: "); 
+        double num1 = scanner.nextDouble(); 
+        System.out.print("Digite o segundo número: "); 
+        double num2 = scanner.nextDouble(); 
+        double soma = num1 + num2; 
+        System.out.println("A soma é: " + soma); 
+        scanner.close(); 
     }
 }
